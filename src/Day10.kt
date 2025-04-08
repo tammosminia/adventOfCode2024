317d14b3f664dccbcbedf25487a331aa48bef5c2
tammosminia <tammosminia@gmail.com>
2025-04-08 13:57:29 +0200

    day 10

---
+++ src/Day10.kt
@@ -0,0 +1,158 @@
+import Day10.parse
+import Day10.run1
+import Day10.run2
+import kotlin.math.max
+import kotlin.math.min
+
+object Day10 {
+    fun <E> List<E>.update(index: Int, e: E): List<E> =
+        this.take(index) + e + this.drop(index + 1)
+
+    data class Coordinate(val x: Int, val y: Int) {
+        companion object {
+            val straightDirections = setOf(Coordinate(-1, 0), Coordinate(0, -1), Coordinate(0, 1), Coordinate(1, 0))
+            val diagonalDirections = setOf(Coordinate(-1, -1), Coordinate(-1, 1), Coordinate(1, -1), Coordinate(1, 1))
+            val allDirections = straightDirections + diagonalDirections
+        }
+        operator fun plus(c: Coordinate): Coordinate = Coordinate(x + c.x, y + c.y)
+        operator fun minus(c: Coordinate): Coordinate = Coordinate(x - c.x, y - c.y)
+    }
+    data class Grid<E>(val m: List<List<E>>) {
+        companion object {
+            fun <E> parse(input: String, parseLine: (String) -> List<E>): Grid<E> =
+                Grid(input.lines().filter { it.isNotBlank() }.map(parseLine))
+
+            fun parseCharGrid(input: String): Grid<Char> =
+                parse(input) { it.toCharArray().toList() }
+        }
+
+        fun width(): Int = m.first().size
+        fun height(): Int = m.size
+
+        fun get(c: Coordinate): E = m[c.y][c.x]
+        fun getSafe(c: Coordinate): E? = m.getOrNull(c.y)?.getOrNull(c.x)
+
+        fun set(c: Coordinate, value: E): Grid<E> =
+            Grid(m.update(c.y, m[c.y].update(c.x, value)))
+
+        fun allCoordinates(): List<Coordinate> =
+            0.rangeUntil(height()).flatMap { y ->
+                0.rangeUntil(width()).map { x ->
+                    Coordinate(x, y)
+                }
+            }
+
+        fun isInside(c: Coordinate): Boolean =
+            c.x >= 0 && c.x < width() && c.y >= 0 && c.y < height()
+
+        fun findAll(e: E): List<Coordinate> =
+            findAll { it == e }
+        fun findAll(f: (E) -> Boolean): List<Coordinate> =
+            allCoordinates().filter { f(get(it)) }
+
+        fun <R> map(f: (E) -> R): Grid<R> =
+            Grid(m.map { it.map(f) })
+    }
+
+    @JvmInline value class Height(val value: Int) {
+        fun inc(by: Int = 1) = Height(value + by)
+    }
+
+    fun parse(input: String): Grid<Height> =
+        Grid.parseCharGrid(input).map { Height(it.digitToInt()) }
+
+    fun trailheadScore(g: Grid<Height>, trailhead: Coordinate): Int {
+        fun rec(c: Coordinate): Set<Coordinate> {
+            val height = g.get(c)
+            return if (height.value == 9) setOf(c)
+                else Coordinate.straightDirections
+                    .map { c + it }
+                    .filter { g.getSafe(it) == height.inc() }
+                    .flatMap { rec(it) }
+                    .toSet()
+        }
+        return rec(trailhead).size
+    }
+
+    fun run1(input: Grid<Height>): Int =
+        input.findAll(Height(0)).sumOf { trailheadScore(input, it) }
+
+    fun trailheadRating(g: Grid<Height>, trailhead: Coordinate): Int {
+        fun rec(c: Coordinate): Int {
+            val height = g.get(c)
+            return if (height.value == 9) 1
+            else Coordinate.straightDirections
+                .map { c + it }
+                .filter { g.getSafe(it) == height.inc() }
+                .sumOf { rec(it) }
+        }
+        return rec(trailhead)
+    }
+
+    fun run2(input: Grid<Height>): Int =
+        input.findAll(Height(0)).sumOf { trailheadRating(input, it) }
+}
+
+fun main() {
+    val input = """
+        12340101878761023456545454342165345676769343212898101234
+        01253298999452310897102367443078756789878552100347610945
+        10765387888341456798211078954569865498765667891256329876
+        67876456672210878654302234567054654328654308760345434565
+        58960145521008969543033123898123521019603219874321294423
+        87654234432023457652123087878931438908712387665230783212
+        94543454342112348956542198963210567125654498540145632101
+        23612763543201032347899017654231436034675643489876543234
+        18703892698715101051058720140145621894589732106789012105
+        09543211789876510962345638230898730783289801875498721076
+        67657600692105401873496549821769845670170789964329630387
+        78998776543212392398787056765654301234561276353010541298
+        12389985430124985407652145654510210019654305442126752987
+        01276578921003476014550234783323212128763012330145893456
+        10345667654312564323761221092104303499892100123232123454
+        67812456783239875019854332112365694781012349874123010763
+        54903441092128756998543345001436785632509854365032109894
+        43786532361045677877632356916512345683408765256743238876
+        32697895678738988988901987887402398796512120129854567905
+        01521004569827432234870676590341456587543098218769871014
+        10435410458216541123766783411250107079650187601256712323
+        28976321326502100054355692101763298128756234532345603478
+        67689100015489232360544543439874380137897878943210436569
+        86543212326376541071239876528545673249856967657654347878
+        98743265417217634289140345012789654356745667898943256981
+        01650178108908987123021236109878766325633458987001107210
+        32210989832212456014123987216765645414722189076122098723
+        43345676541003327865474532125456736703810012165433489654
+        56934307687654018976789643034143821892954187234454967503
+        67855218998987652189888752102012960911063296545567818912
+        12346783219878941079892167811023451103478103456765401876
+        01656594100568932016783078912110167012569812343890322345
+        10789015601457657825654189801256778929658751012701210154
+        03498323712310146934568789221349877878743567801662567030
+        12567487893401235476549870130210918969812478945673498121
+        03456596987632123389034765245601101255702301234989187432
+        12456745076541034232125634310781056344321030123231016589
+        12349832125765015121034567323892347897812345678192354678
+        01901221034894176076543278498787323458900156569087463210
+        10876320676783289089432109586565510067851287438196578987
+        76505410787690987128761012675676432104560399821045678876
+        85414125698541056298456783254986549023870456734430019565
+        98323034567232345347321292123877678016901960109821023432
+        86018601256123453456510398001678987655432871218783456501
+        67889543345034762387105467210900178746945982345692167898
+        56977602358903891093456554367810269637876672107898056982
+        43868911567212987784367893456524341023776543206561154321
+        32154320126303456101210210347433452014789432015210067010
+        41032101015454543289010101298934563128128721324332198011
+        32349876522387635376521032876129654789013210458949876523
+        21456910431497656785438945965098765658134502167653210435
+        10237801230598547898747876234876666543265653057894387654
+        90198212347654632336656760143965455698678784567765896023
+        83285675498103201345698601107834564789549894398756895112
+        74234786987230110218767652216723873210432765212347763203
+        65101697896541222109856543345010960123101656101078954512
+    """.trimIndent()
+    val parsed = parse(input)
+    println(run1(parsed))
+    println(run2(parsed))
+}
---
+++ test/Day10Test.kt
@@ -0,0 +1,31 @@
+import Day10.parse
+import Day10.run1
+import Day10.run2
+import Day10.trailheadScore
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day10Test {
+    val sample = """
+        89010123
+        78121874
+        87430965
+        96549874
+        45678903
+        32019012
+        01329801
+        10456732
+    """.trimIndent()
+    val input = parse(sample)
+    @Test
+    fun test1() {
+        assertEquals(1, trailheadScore(input, Day10.Coordinate(2, 5)))
+        assertEquals(3, trailheadScore(input, Day10.Coordinate(5, 5)))
+        assertEquals(36, run1(input))
+    }
+    @Test
+    fun test2() {
+        assertEquals(81, run2(input))
+    }
+
+}
